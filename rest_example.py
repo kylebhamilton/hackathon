3c5b2c7b682485481265d9d7afa388311a0746a7
root <root@baracudas-tester>
2017-03-09 17:01:45 -0500

    add get pm function

--- rest_example.py
+++ rest_example.py
@@ -49,6 +49,101 @@ def get(relative_url):
 def print_as_json(data, indent=2):
   print json.dumps(data, indent=indent)
 
+def get_pm(uri):
+  ##uri to interface
+  ##example /mit/me/1/eqh/shelf,%s/eqh/slot,%s/eq/card/ptp/nw,1/opt
+
+  #GET INTERFACE NAME
+  dict= get(uri)
+  interface_name=dict["fnm"]
+
+  #GET MONITORING LAYER NAME
+  pm=dict["pm"]
+  pm_layer=pm["pment"]
+
+  #GET ALL PM VALUES
+  pm_uri="%s/pm/crnt" % uri
+  dict= get(pm_uri)
+
+  #FIND CURRENT PMs (nint)
+  list=dict["result"]
+  list_with_all_nint_pms= [element for element in list if element['bintv'] == 'nint']
+  dict_with_all_nint_pms=list_with_all_nint_pms[0]
+
+  #GET PM DATA
+  bintv=dict_with_all_nint_pms["bintv"]
+  pm_profile_name=dict_with_all_nint_pms["name"]
+  dict_pm=dict_with_all_nint_pms["pmdata"]
+
+  opr=dict_pm["opr"]
+  opt=dict_pm["opt"]
+
+  #GET OPT PM PROFILE
+  uri="/mit/me/1/pmprof/%s,%s,%s" % (pm_layer, bintv, pm_profile_name)
+  dict=get(uri)
+  list=dict["monspec"]
+
+  #GET OPR TCA
+  list_opr= [element for element in list if element['montyp'] == 'opr']
+  dict_opr=list_opr[0]
+  #print dict_opr
+  montype=dict_opr["montyp"]
+  opr_l=dict_opr["thrl"]
+  opr_h=dict_opr["thrh"]
+
+  #GET OPT TCA
+  list_opt= [element for element in list if element['montyp'] == 'opt']
+  dict_opt=list_opt[0]
+  montype=dict_opt["montyp"]
+  opt_l=dict_opt["thrl"]
+  opt_h=dict_opt["thrh"]
+
+  #BUILD DICTIONARY FROM ALL DATAS
+  pm_dict = {}
+  pm_dict['opr_l'] = opr_l
+  pm_dict['opr_h'] = opr_h
+  pm_dict['opt_l'] = opt_l
+  pm_dict['opt_h'] = opt_h
+  pm_dict['opr'] = opr
+  pm_dict['opt'] = opt
+  pm_dict['if_name'] = interface_name
+
+  #print pm_dict
+  return pm_dict
+
+#SYSTEM
+ip="10.16.24.50"
+shelf_id=1
+login(ip)
+
+#QFLEX NETWORK
+slot_id=1
+port="nw"
+uri="/mit/me/1/eqh/shelf,%s/eqh/slot,%s/eq/card/ptp/%s,1/opt" % (shelf_id,slot_id,port)
+dict=get_pm(uri)
+print dict
+
+#QFLEX CLIENT OPTM - currently broken
+slot_id=1
+port="cl"
+uri="/mit/me/1/eqh/shelf,%s/eqh/slot,%s/eq/card/ptp/%s,1/optm/optl/1" % (shelf_id,slot_id,port)
+dict=get_pm(uri)
+print dict
+
+#AMP NETWORK
+slot_id=2
+port="nw"
+uri="/mit/me/1/eqh/shelf,%s/eqh/slot,%s/eq/card/ptp/%s,1/opt" % (shelf_id,slot_id,port)
+dict=get_pm(uri)
+print dict
+
+#AMP CLIENT
+slot_id=2
+port="cl"
+uri="/mit/me/1/eqh/shelf,%s/eqh/slot,%s/eq/card/ptp/%s,1/opt" % (shelf_id,slot_id,port)
+dict=get_pm(uri)
+print dict
+
 """
 from rest_example import *
 login("10.16.24.9")
